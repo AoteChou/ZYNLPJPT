03a5be588b0981852d66e2fed708c3ff6bb07bba
jessenpan <jessenpan@gmail.com>
2014-04-28 22:40:22 +0800

    add function of deleteAllZyEjzb

---
+++ sczyejzb.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ZYNLPJPT.DAL;
+using ZYNLPJPT.Model;
+
+namespace ZYNLPJPT
+{
+    public partial class sczyejzb : System.Web.UI.Page
+    {
+        protected XyXkZyView[] xyXkZyViews;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["yh"] == null)
+            {
+                this.Response.Redirect("Default.htm");
+            }
+            else
+            {
+                YH yh = (YH)Session["yh"];
+                //验证用户是否是教师角色,无则没有配置权限
+                YHJSView yhjsView = new YHJSView_DAL().GetModel(yh.YHBH.Trim());
+                if (yhjsView.JSM.Trim() != "教师")
+                {
+                    Response.Redirect("ErrorPage.aspx?msg=对不起，系统配置出错，你没有配置专业二级指标的权利&fh=false");
+                }
+                else
+                {
+                    int xkbh = new JSTea_DAL().GetModel(yh.YHBH.Trim()).SSXK;
+                    xyXkZyViews = new XyXkZyView_DAL().getCKArray(xkbh);
+                }
+            }
+        }
+    }
+}
